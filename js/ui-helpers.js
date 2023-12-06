e6bd1464a399a52c99427e503680e5e403f5ee70
Vicente <vicenteferrer_01@hotmail.com>
2023-12-06 10:46:24 -0300

    Improved lastUpdate

--- js/ui-helpers.js
+++ js/ui-helpers.js
@@ -25,7 +25,7 @@ function reset() {
 
 function lastUpdateTemplate(date) {
   const lastUpdate = datediff(date)
-  return `Last update: ${(lastUpdate ? `${lastUpdate} ago` : 'Not available')}`
+  return `Last update: ${(lastUpdate ? `${lastUpdate} ago` : 'Recently')}`
 }
 
 function linkSetUp(linkElement, text, url) {
